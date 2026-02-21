0432bfe931c04de9ef06d645bc22254d3f79a49e
philochardbleu <37799557+philochardbleu@users.noreply.github.com>
2026-02-21 10:22:55 +0100

    Add files via upload

--- ESP32Kiln_MQTT.ino
+++ ESP32Kiln_MQTT.ino
@@ -2,10 +2,12 @@
 #include <PubSubClient.h>
 
 extern double kiln_tmp1;
+extern PROGRAM_RUN_STATE Program_run_state;  // type défini dans ESP32Kiln.h
+
 bool ha_discovery_sent = false;
-const char* mqtt_server = "server adress";
-const char* mqtt_user = "server user";
-const char* mqtt_password = "server password";
+const char* mqtt_server = "192.168.1.5";
+const char* mqtt_user = "philochardbleu";
+const char* mqtt_password = "Gouph123";
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
@@ -17,11 +19,47 @@ bool mqtt_started = false;
 
 // ----------------------------------
 
+// ----------------------------------
+// Callback MQTT : reçoit les commandes de Home Assistant
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+  String msg;
+  for (unsigned int i = 0; i < length; i++) msg += (char)payload[i];
+  msg.trim();
+
+  Serial.print("[MQTT] <- ");
+  Serial.print(topic);
+  Serial.print(" : ");
+  Serial.println(msg);
+
+  if (String(topic) == "pidkiln/command") {
+    if (msg == "START") {
+      Serial.println("[MQTT] Démarrage demandé par HA");
+      if (Program_run_state == 3) RESUME_Program();  // PR_PAUSED = 3
+      else START_Program();
+      mqttClient.publish("pidkiln/state", "ON", true);
+    } else if (msg == "STOP") {
+      Serial.println("[MQTT] Arrêt demandé par HA");
+      END_Program();
+      mqttClient.publish("pidkiln/state", "OFF", true);
+    } else if (msg == "PAUSE") {
+      Serial.println("[MQTT] Pause demandée par HA");
+      PAUSE_Program();
+      mqttClient.publish("pidkiln/state", "PAUSED", true);
+    } else if (msg == "ABORT") {
+      Serial.println("[MQTT] Abandon demandé par HA");
+      ABORT_Program(5);  // PR_ERR_USER_ABORT = 5 - vérifier dans ESP32Kiln.h
+      mqttClient.publish("pidkiln/state", "OFF", true);
+    }
+  }
+}
+
+// ----------------------------------
 void mqtt_init_once() {
   if (mqtt_started) return;
 
   mqttClient.setServer(mqtt_server, 1883);
-  mqttClient.setBufferSize(512);  // ← AJOUTEZ CETTE LIGNE
+  mqttClient.setBufferSize(512);
+  mqttClient.setCallback(mqtt_callback);
   mqtt_started = true;
 
   Serial.println("[MQTT] init");
@@ -34,7 +72,9 @@ void mqtt_reconnect() {
 
   if (mqttClient.connect("PIDKiln", mqtt_user, mqtt_password)) {
     Serial.println("[MQTT] connecté");
-      ha_discovery_sent = false;  // ← Permet de renvoyer le discovery
+    ha_discovery_sent = false;
+    mqttClient.subscribe("pidkiln/command");
+    Serial.println("[MQTT] abonné à pidkiln/command");
   }
 }
 
@@ -140,6 +180,21 @@ void mqtt_publish_discovery()
     payload,
     true);
 
+  // ---- Switch Start/Stop ----
+  snprintf(payload, sizeof(payload),
+    "{"
+    "\"name\":\"PIDKiln Démarrage\","
+    "\"command_topic\":\"pidkiln/command\","
+    "\"state_topic\":\"pidkiln/state\","
+    "\"payload_on\":\"START\","
+    "\"payload_off\":\"STOP\","
+    "\"state_on\":\"ON\","
+    "\"state_off\":\"OFF\","
+    "\"unique_id\":\"pidkiln_switch\","
+    "\"retain\":true"
+    "}"
+  );
+  mqttClient.publish("homeassistant/switch/pidkiln/start/config", payload, true);
+
   Serial.println("[MQTT] discovery HA publié");
 }
-
